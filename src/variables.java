cabe3514e2a2a54eaf27f7ac6465cd9ad341e33a
Kevin-windows-11 <kevin.alvarenga93@gmail.com>
2023-02-04 10:25:27 -0600

    primer commit

---
+++ src/variables.java
@@ -0,0 +1,9 @@
+public class variables {
+    public static void main(String[] args) {
+        //Declarando variable enteros y la inicializamos en 0
+        int speed = 0;
+        System.out.println(speed);
+
+    }
+}
+
