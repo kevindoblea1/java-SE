0b9f613d869fffa79249e79f4bdad6fc3845b500
Kevin Alvarenga <kevin.alvarenga93@gmail.com>
2023-02-06 09:36:07 -0600

    Variables y funciones

--- src/variables.java
+++ src/variables.java
@@ -1,9 +1,24 @@
+import javax.swing.*;
+
 public class variables {
     public static void main(String[] args) {
-        //Declarando variable enteros y la inicializamos en 0
-        int speed = 0;
+        //Declarando variabl
+        int speed;
+        //inicializamos una variable
+        speed = 10;
+        //imprimimos una variable
         System.out.println(speed);
 
+        //creamos variables para llamar una funcion
+        String employeeName = "Kevin";
+        int salary = 1360;
+        //llamamos a la funcion
+        ImprimeSalario(employeeName, salary);
+
+    }
+    static void ImprimeSalario(String employeeName, int salary)
+    {
+        System.out.println("Salario del empleado " + employeeName + " es de $ " + salary);
     }
 }
 
